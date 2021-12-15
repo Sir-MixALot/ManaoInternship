03628173bbce3af99a31fe8d3f41867b032a8b45
Sergey <sergey.gorshkov777@gmail.com>
2021-12-15 17:34:41 +0300

    Complete task #1.2

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 
+$n = 37;
+if($n>36){
+    echo "Нет 4-значного числа, сумма цифр которого будет равна " . $n;
+}
+for($i=1000; $i<=9999; $i++){
+    $first = floor($i/1000);
+    $sec = floor(($i%1000)/100);
+    $third = floor(($i%100)/10);
+    $fourth = $i%10;
+    $sum = $first+$sec+$third+$fourth;
+    if($sum == $n){
+        echo $i . "</br>";
+    }
+}
