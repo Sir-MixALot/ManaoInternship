60501eee0fd939d20d6ae463bddccc93731eacd4
Sergey <sergey.gorshkov777@gmail.com>
2021-12-16 10:28:19 +0300

    Complete task 1.4

--- index.php
+++ index.php
@@ -2,5 +2,17 @@
 
 //Task 1.4
 
-
+for($i=1000; $i<=9999; $i++){
+    if($i%2 == 0){
+        $first = floor($i/1000);
+        $sec = floor(($i%1000)/100);
+        $third = floor(($i%100)/10);
+        $fourth = $i%10;
+        if($first<$sec && $sec<$third && $third<$fourth){
+            echo $i . ' - возрастающая последовательность </br>';
+        }else if($first>$sec && $sec>$third && $third>$fourth){
+            echo $i . ' - убывающая последовательность </br>';
+        }
+    }
+}
 
