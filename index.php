b87d6df9c7a0f93ebc31861e93b8c37852e50771
Sergey <sergey.gorshkov777@gmail.com>
2021-12-16 09:08:48 +0300

    Create 3rd task

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
 <?php
 
-//Task 1.1
+//Task 1.1 v2
 
 $x = 5;
 $N = 5455;
-$count = 0;
 echo 'Ваше число: ' .$N. '</br>';
 
-function getCount($x, $N, $count)
-{
-    if($N%10 == $x){
-        $count+=1;
-    }
-    if($N/10>1){
-        return getCount($x, floor($N/10), $count);
-    }
+for($i=$N; $i=0; $i=$i/10){
+    $count = 0;
+    echo $i;
+//    if($i%10==$x){
+//        $count+=1;
+//    }
     echo 'Количество цифр, равных'.$x.': ' . $count;
 }
-echo getCount($x, $N, $count);
+
+//function getCount($x, $N, $count)
+//{
+//    if($N%10 == $x){
+//        $count+=1;
+//    }
+//    if($N/10>1){
+//        return getCount($x, floor($N/10), $count);
+//    }
+//    echo 'Количество цифр, равных'.$x.': ' . $count;
+//}
+//echo getCount($x, $N, $count);
 
 //
 
