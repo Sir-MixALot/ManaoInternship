7d950535f9b10e7a57eb9a80784c43db80b9607e
Sergey <sergey.gorshkov777@gmail.com>
2021-12-17 14:29:18 +0300

    Complete task

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
 
-//Task 1.9
+//Task 1.10
 
-$N = 5;
+$N = 100;
 echo $N . " - ваше число</br>";
-$NCount  = ceil(log10($N));
-$square =  $N**2;
-echo $square . " - квадрат вашего числа</br>";
-if($square%(10**$NCount) == $N){
-    echo 'Число является автоморфным';
+if(floor($N/10)==0){
+    echo "Введите число больше одной цифры";
 }else{
-    echo 'Число не является автоморфным';
+    $count = 0;
+    for($i=$N-1;$i>=10;$i--){
+        if(getPalindrome($i)){
+            $square =  $i**2;
+            if(getPalindrome($square)){
+                $count+=1;
+                echo $i . " - это палиндром</br>";
+                echo $square . " - квадрат числа - это тоже палиндром</br>";
+            }
+
+        }
+
+    }
+    if($count == 0){
+        echo "Нет чисел меньше вашего, которые явл палиндромом</br>";
+    }
 }
 
+function getPalindrome($number)
+{
+    for($j=$number; $j>1; floor($j/=10)){
+        $palindrome = $palindrome*10+($j%10);
+    }
+    if($palindrome == $number){
+        return true;
+    }else{
+        return false;
+    }
+}
