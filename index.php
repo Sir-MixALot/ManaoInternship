08ee110a6dc8c78aae556593a2016ca0ca42bcae
Sergey <sergey.gorshkov777@gmail.com>
2021-12-17 11:41:46 +0300

    Create task

--- index.php
+++ index.php
@@ -1,29 +1,26 @@
 <?php
 
-//Task 1.7
-
-$N = 3173;
-echo 'Ваше число: ' .$N. '</br>';
-if(floor($N/10)==0){
-    echo "Введите число больше одной цифры";
-}else{
-    $equal = false;
-    for($i = $N; $i>10; floor($i/=10)){
-
-        for($j = $i; $j>10; floor($j/=10)){
-            if($i%10 == floor(($j%100)/10)){
-                $equal = true;
-                break;
-            }
-        }
-        if($equal){
+//Task 1.8
+$notEqual = true;
+for($i=1002; $i<=1002; $i++){
+    echo $i . '</br>';
+    for($j = $i; $j>10; floor($j/=10)){
+        echo $i%10 . '/' . floor(($j%100)/10) . '</br>';
+        if($i%10 != floor(($j%100)/10)){
+            $notEqual = true;
+        }else{
             break;
         }
     }
-    if($equal){
-        echo 'Число содержит две одинаковые цифры';
+    if(!$notEqual){
+        break;
+    }
+    if($notEqual){
+        echo $i . 'Число содержит две одинаковые цифры';
     }else{
         echo 'Число не содержит одинаковых цифр';
     }
 
 }
+
+
