8e51c3ed9cdb8794a8d444b81d2ccbee31a25ba1
Sergey <sergey.gorshkov777@gmail.com>
2021-12-17 12:11:36 +0300

    Complete task

--- index.php
+++ index.php
@@ -2,23 +2,24 @@
 
 //Task 1.8
 $notEqual = true;
-for($i=1002; $i<=1002; $i++){
-    echo $i . '</br>';
-    for($j = $i; $j>10; floor($j/=10)){
-        echo $i%10 . '/' . floor(($j%100)/10) . '</br>';
-        if($i%10 != floor(($j%100)/10)){
-            $notEqual = true;
-        }else{
+for($y=1000; $y<=9999; $y++){
+    $equal = false;
+    for($i = $y; $i>10; floor($i/=10)){
+
+        for($j = $i; $j>10; floor($j/=10)){
+            if($i%10 != floor(($j%100)/10)){
+                $equal = false;
+            }else{
+                $equal = true;
+                break;
+            }
+        }
+        if($equal){
             break;
         }
     }
-    if(!$notEqual){
-        break;
-    }
-    if($notEqual){
-        echo $i . 'Число содержит две одинаковые цифры';
-    }else{
-        echo 'Число не содержит одинаковых цифр';
+    if(!$equal){
+        echo $y . '</br>';
     }
 
 }
