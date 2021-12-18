1bd0a0fe59e56da7a8b2247408377e4a80655d54
Sergey <sergey.gorshkov777@gmail.com>
2021-12-18 14:24:42 +0300

    complete task

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-//Task 1.16
+//Task 1.18
 
-$N = 1;
-$M = 10000;
+$N = 220;
+$M = 6000;
 echo $N . ' ваше первое число</br>';
 echo $M . ' ваше второе число</br>';
 if(gettype($N) == "double" || gettype($M) == "double"){
@@ -14,26 +14,35 @@ if(gettype($N) == "double" || gettype($M) == "double"){
     }else if ($N>=$M){
         echo 'Первое число должно быть меньше второго';
     }else{
-        checkPerfect($N, $M);
-    }
+        checkFriendlyNumbers($N, $M);
+        }
 }else{
     echo 'Введите числа, отличные от нуля';
 }
 
-function checkPerfect($n, $m)
+function checkFriendlyNumbers($n, $m)
 {
-    for ($i = $n; $i < $m; $i++)
-    {
-        $half = floor($i/2);
-        $sum = 0;
-        for($j = 1; $j <= $half; $j++){
-            if ($i % $j == 0)
-            {
-                $sum += $j;
+    for($i=$n; $i<$m; $i+=60){
+        for($j=$i+1; $j<$m; $j++){
+            if(getDeviders($i)==$j){
+                if(getDeviders($j)==$i){
+                    echo $i . '+' . $j . '</br>';
+                }
             }
         }
-        if($sum == $i){
-            echo $i . '/';
-        }
     }
+}
+
+function getDeviders($x)
+{
+        $half = floor($x/2);
+        $sum = 0;
+        for($y = 1; $y <= $half; $y++){
+            if ($x % $y == 0)
+            {
+                $sum += $y;
+            }
+        }
+        return $sum;
+
 }
