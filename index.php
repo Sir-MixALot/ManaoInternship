8a43d29f84c6fb4c8fd13f76fc087ff90d9b51bb
Sergey <sergey.gorshkov777@gmail.com>
2021-12-18 11:32:27 +0300

    complete task

--- index.php
+++ index.php
@@ -1,28 +1,39 @@
 <?php
 
-//Task 1.15
+//Task 1.16
 
-$N = -15;
-echo $N . ' ваше число</br>';
-if(gettype($N) == "double"){
-    echo 'Введите целое число';
-}else if($N<>0){
-    getDeviders($N);
+$N = 1;
+$M = 10000;
+echo $N . ' ваше первое число</br>';
+echo $M . ' ваше второе число</br>';
+if(gettype($N) == "double" || gettype($M) == "double"){
+    echo 'Введите целые числа';
+}else if($N<>0 && $M<>0){
+    if($N<0 || $M<0){
+        echo 'Введите положительные числа';
+    }else if ($N>=$M){
+        echo 'Первое число должно быть меньше второго';
+    }else{
+        checkPerfect($N, $M);
+    }
 }else{
-    echo 'Введите число, отличное от нуля';
+    echo 'Введите числа, отличные от нуля';
 }
 
-function getDeviders($n)
+function checkPerfect($n, $m)
 {
-    $m=1;
-    if($n<0){
-        $m = -1;
-    }
-    for ($i = 1; $i <= abs($n); $i++)
+    for ($i = $n; $i < $m; $i++)
     {
-        if ($n % $i == 0)
-        {
-            echo ($i*$m) . ' / ';
+        $half = floor($i/2);
+        $sum = 0;
+        for($j = 1; $j <= $half; $j++){
+            if ($i % $j == 0)
+            {
+                $sum += $j;
+            }
+        }
+        if($sum == $i){
+            echo $i . '/';
         }
     }
 }
