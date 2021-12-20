91be71a4df5dec798c049d2179d93af34368ccfd
Sergey <sergey.gorshkov777@gmail.com>
2021-12-20 17:20:36 +0300

    complete task

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
 
-//Task 1.1
+//Task 1.25
 
-$x = 5;
-$N = 5455;
+$n = 1;
+$m = 50000;
+echo $n.' - стартовое число</br>';
+echo $m.' - количество чисел</br>';
+$sum = 0;
 $count = 0;
-echo 'Ваше число: ' .$N. '</br>';
+for($i=$n; $m>0;  $i++){
+    $m--;
+    $factorialSum = 0;
+    for($j = $i; $j>0;$j=floor($j/10)){
+        $factorialSum += factorial($j%10);
+    }
+    if($i == $factorialSum){
+        $sum+=$i;
+        $count++;
+        echo $i .'-'. $factorialSum . '</br>';
+    }
 
-function getCount($x, $N, $count)
-{
-    if($N%10 == $x){
-        $count+=1;
-    }
-    if($N/10>1){
-        return getCount($x, floor($N/10), $count);
-    }
-    echo 'Количество цифр, равных'.$x.': ' . $count;
 }
-echo getCount($x, $N, $count);
+echo 'Количество чисел: ' . $count . '. Сумма чисел: ' . $sum;
 
-//
-
+function factorial($n)
+{
+    if ($n <= 1) return 1;
+    return $n * factorial($n - 1); // здесь происходит повторный вызов функции
+}
