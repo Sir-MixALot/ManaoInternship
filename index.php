c6452ff04a20faed76d5401ca5af0e93dc8c13ef
Sergey <sergey.gorshkov777@gmail.com>
2021-12-26 11:50:51 +0300

    complete and reworked task

--- index.php
+++ index.php
@@ -1,41 +1,88 @@
 <?php
+//task 1.29
 
-//Task 1.29
+$n=954632187;
+$m=30;
+$k=44;
+$changeCounter=0;
+$numberPrototype=0;
+$increaseNumberPrototype=0;
 
-$n = 99;
-$m = 15;
-$k = 3;
-echo $n.' - стартовое число</br>' . $m.' - количество чисел</br>' . $k . ' - специальное число</br>';
+echo $n.' -стартовое число</br>'.$m.' -количество чисел</br>'.$k.' -специальное число</br>';
 
 for($i=$n;$m>0;$i++){
     $m--;
-    $can = true;
-    $nCount = ceil(log($i, 10));
-    if($nCount>2){
-        $can = (checkSum($i, $k));
-        if(!$can){
-            echo $i. ' - нельзя</br>';
-        }else{
-            echo $i. ' - можно</br>';
+    $numberPrototype=$i;
+    $nCount = ceil(log($numberPrototype, 10));
+    if($nCount>1){
+        $increaseNumber=getIncreaseNumber($numberPrototype);
+        $increaseNumberPrototype=$increaseNumber;
+        for($j=$changeCounter;true;$j=checkChangedSum($increaseNumberPrototype,$k,$changeCounter)){
+            if($j==-1){
+                echo $numberPrototype.' - можно</br>';
+                break;
+            }else if($j+1==ceil(log($numberPrototype,10))){
+                echo $numberPrototype.' - нельзя</br>';
+                break;
+            }
         }
     }else{
-        echo $i . ' - нельзя</br>';
+        echo $numberPrototype . ' - нельзя</br>';
     }
 }
 
-function checkSum($n, $k)
+
+
+function checkChangedSum($number, $k, $kCount)
 {
-    $wholeSum = 0;
-    for($j=$n; $j>0;$j=floor($j/10)){
-        $wholeSum +=$j%10;
-        if($j%10 == $k){
-            return false;
+    global $numberPrototype, $increaseNumberPrototype;
+    if(getNumberSum($increaseNumberPrototype)>$k){
+        if(ceil(log($increaseNumberPrototype,10))>1){
+            $changedNumber=(floor($increaseNumberPrototype/(10**$kCount))*(10**(abs(1-$kCount))))+$increaseNumberPrototype%(10**(abs(1-$kCount)));
+            if($number%10>$k){
+                if(ceil(log($number,10))>1){
+                    return checkChangedSum(floor($number/10),$k , $kCount);
+                }else{
+                    $increaseNumberPrototype = $changedNumber;
+                }
+            }else if($number%10<$k){
+                if(ceil(log($number,10))>1){
+                    return checkChangedSum(floor($number/10),$k-($number%10),$kCount+=1);
+                }else{
+                    $increaseNumberPrototype = $changedNumber;
+                }
+            }else if($number%10==$k){
+                return -1;
+            }
+        }else{
+            return ceil(log($numberPrototype,10))-1;
+        }
+    }else if(getNumberSum($increaseNumberPrototype)==$k){
+        return -1;
+    }else{
+        return ceil(log($numberPrototype,10))-1;
+    }
+}
+
+function getIncreaseNumber($number)
+{
+    $increaseNumber=0;
+    for($x = 1; $x < 10; $x++)
+    {
+        for($y=$number; $y>0; $y=floor($y/10)){
+            if($y%10==$x){
+                $increaseNumber = ($increaseNumber*10)+$x;
+            }
         }
     }
-    if($wholeSum<=$k){
-        return false;
-    }else{
-        return true;
-    }
+    return $increaseNumber;
+}
 
+function getNumberSum($number)
+{
+    $sum = 0;
+    for($i=$number; $i>0;$i=floor($i/10)){
+        $sum +=$i%10;
+    }
+    return $sum;
 }
