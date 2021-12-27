ce3afae5a2f88073bd915ec70d8abc9ad9410522
Sergey <sergey.gorshkov777@gmail.com>
2021-12-27 09:37:52 +0300

    Complete task

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
 
-//Task 1.1
-
-$x = 5;
-$N = 5455;
-$count = 0;
-echo 'Ваше число: ' .$N. '</br>';
-
-function getCount($x, $N, $count)
-{
-    if($N%10 == $x){
-        $count+=1;
+//Task 1.30
+$n = 1000;
+$m = (2*$n)+1;
+for($i=$n; $i<=$m; $i++){
+    if(isSimpleNumber($i)){
+            if(isSimpleNumber($i+2)){
+                echo $i . '+' . ($i+2) . '</br>';
+            }
     }
-    if($N/10>1){
-        return getCount($x, floor($N/10), $count);
-    }
-    echo 'Количество цифр, равных'.$x.': ' . $count;
 }
-echo getCount($x, $N, $count);
 
-//
+function isSimpleNumber($n)
+{
+    $simple = true;
 
+
+    $SquareRoot = floor(sqrt($n));
+    for ($i = 2; $i <= $SquareRoot; $i++)
+    {
+        if ($n % $i == 0)
+        {
+            $simple = False;
+            break;
+        }
+    }
+    return $simple;
+
+}
