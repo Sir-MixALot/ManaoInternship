ace276e14b25dafc89f6e7366316777534898f50
Sergey <sergey.gorshkov777@gmail.com>
2021-12-27 12:50:54 +0300

    reworked task

--- index.php
+++ index.php
@@ -3,23 +3,24 @@
 //Task 2.1
 
 $a = [1,0,2,3,-2,5,8,6,-9,7,1,6];
+echo '<pre>' . print_r($a, true) . '</pre>';
 $count=0;
 $sum=0;
-$b = [];
-for($i=0;$i<=count($a);$i++){
-    echo $a[$i].'/';
-    if($a[$i]<0){
-        $b[]=$i;
+for($i=0;$i<count($a);$i++){
+    if($a[$i]>=0){
+        $count++;
+    }else{
+        break;
     }
     
 }
 
-for($j=0;$j<$b[0];$j++){
-    $count++;
-}
-
-for($y=end($b)+1;$y<count($a);$y++){
-    $sum+=$a[$y];
+for($y=count($a)-1;$y>=0;$y--){
+    if($a[$y]>0){
+         $sum+=$a[$y];          
+    }else{
+        break;
+    }
 }
 
 echo '</br>Количество элементов до первого отрицательного числа = '.$count.'</br>';
