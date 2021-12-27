07b503fdf14376034345f26a1067f8862478b8be
Sergey <sergey.gorshkov777@gmail.com>
2021-12-27 15:42:19 +0300

    reworked

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 //task 1.29
 
-$n=246;
-$m=10;
+$n=270;
+$m=150;
 $k=6;
+$count = 0;
 $changeCounter=0;
 $numberPrototype=0;
 $increaseNumberPrototype=0;
@@ -19,6 +20,7 @@ for($i=$n;$m>0;$i++){
         $increaseNumberPrototype=$increaseNumber;
         for($j=$changeCounter;true;$j=checkChangedSum($increaseNumberPrototype,$k,$changeCounter)){
             if($j==-1){
+                $count++;
                 echo $numberPrototype.' - можно</br>';
                 break;
             }else if($j+1==ceil(log($numberPrototype,10))){
@@ -34,28 +36,30 @@ for($i=$n;$m>0;$i++){
 
 function checkChangedSum($number, $k, $kCount)
 {
+    // echo $number.'('.$k.')['.$kCount.']/';
     global $numberPrototype, $increaseNumberPrototype;
-    if(getNumberSum($increaseNumberPrototype)>$k){
-        if(ceil(log($increaseNumberPrototype,10))>1){
+    if((getNumberSum($increaseNumberPrototype)>$k)){
+        if(((ceil(log($increaseNumberPrototype,10))>1))){
             $changedNumber=(floor($increaseNumberPrototype/(10**$kCount))*(10**(abs(1-$kCount))))+$increaseNumberPrototype%(10**(abs(1-$kCount)));
-            if($number%10>$k && $kCount==0){
+            if($number%10>$k){
                 if(ceil(log($number,10))>1){
                     return checkChangedSum(floor($number/10),$k , $kCount);
                 }else{
-                    $increaseNumberPrototype = floor($increaseNumberPrototype/10);
-                }
-                
-            }else if($number%10>$k){
-                if(ceil(log($number,10))>1){
-                    return checkChangedSum(floor($number/10),$k , $kCount);
-                }else{
-                    $increaseNumberPrototype = $changedNumber;
+                    if($kCount==0){
+                        return ceil(log($numberPrototype,10))-1;
+                    }else{
+                        $increaseNumberPrototype = $changedNumber;
+                    }
                 }
             }else if($number%10<$k){
                 if(ceil(log($number,10))>1){
                     return checkChangedSum(floor($number/10),$k-($number%10),$kCount+=1);
                 }else{
-                    $increaseNumberPrototype = $changedNumber;
+                    if($kCount==0){
+                        return ceil(log($numberPrototype,10))-1;
+                    }else{
+                        $increaseNumberPrototype = $changedNumber;
+                    }
                 }
             }else if($number%10==$k){
                 return -1;
@@ -63,7 +67,7 @@ function checkChangedSum($number, $k, $kCount)
         }else{
             return ceil(log($numberPrototype,10))-1;
         }
-    }else if(getNumberSum($increaseNumberPrototype)==$k){
+    }else if((getNumberSum($increaseNumberPrototype)==$k)){
         return -1;
     }else{
         return ceil(log($numberPrototype,10))-1;
@@ -92,3 +96,5 @@ function getNumberSum($number)
     }
     return $sum;
 }
+
+echo '&&&&&&&'.$count;
