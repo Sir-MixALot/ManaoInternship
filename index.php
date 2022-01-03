76d2aa2fed320fe2a1da9b0ffcd51dcf9cb9ba22
Sergey <sergey.gorshkov777@gmail.com>
2022-01-03 15:16:55 +0300

    create

--- index.php
+++ index.php
@@ -1,23 +1,62 @@
 <?php
 
-//Task 1.1
+//partial permutation
 
-$x = 5;
-$N = 5455;
-$count = 0;
-echo 'Ваше число: ' .$N. '</br>';
-
-function getCount($x, $N, $count)
+$a = new Permutation(1235, 4);
+class Permutation
 {
-    if($N%10 == $x){
-        $count+=1;
+    public $n, $m, $x=[], $count;
+
+    public function __construct($n, $m)
+    {
+        $this->x[] = $n;
+        $this->n = array_map('intval', str_split($n));
+        $this->m = $m;
+        $this->count = $this->getAmount();
+        // $this->Permutation();
+        $this->customPermutation();
     }
-    if($N/10>1){
-        return getCount($x, floor($N/10), $count);
+
+    public function Permutation()   
+    {
+        echo "Количество возможных комбинаций: " .($this->getFactorial(count($this->n))/$this->getFactorial(count($this->n)-$this->m));
     }
-    echo 'Количество цифр, равных'.$x.': ' . $count;
+
+    public function getFactorial($z)
+    {
+        if ($z <= 1) return 1; 
+        return $z * $this->getFactorial($z - 1);
+    }
+
+    public function getAmount()
+    {
+        $amount = 1;
+        for($i=count($this->n); $i>(count($this->n)-$this->m);$i--){
+            $amount*=$i;
+        }
+        return $amount;
+    }
+
+    public function customPermutation()
+    {
+        shuffle($this->n);
+            
+        while($this->count>0){
+            foreach($this->x as $value){
+                if($value!=(int)implode($this->n)){
+                    $this->x[] = (int)implode($this->n);
+                    $this->count--;
+                    $this->customPermutation();
+                }else{
+                    continue;
+                }
+               
+            }
+        }
+        
+        // $b = (int)implode($y); 
+        // echo gettype($b);
+        // 
+        // var_dump($this->n);
+    }
 }
-echo getCount($x, $N, $count);
-
-//
-
