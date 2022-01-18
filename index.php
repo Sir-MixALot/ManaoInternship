9468bb8242e438444c912b6f31cb9a350989c151
Sergey <sergey.gorshkov777@gmail.com>
2022-01-18 14:20:58 +0300

    fix php code style and name of variables

--- index.php
+++ index.php
@@ -1,97 +1,91 @@
 <?php
 
 //partial permutation
+// phpinfo();
 
-$x = new Permutation('12345', 3);
+$result = new Permutation('12345', 3);
 class Permutation
 {
-    public $a, $k, $outString, $count;
+    public $inString;
+    public $subset;
+    public $outString;
+    public $count;
 
-    public function __construct($a, $k)
+    public function __construct($inString, $subset)
     {
-        
-        try{
-            $this->checkString($a);
-            $this->a = str_split($a);
-            $this->checkKey($k);
-            $this->k = $k;
+        try {
+            $this->checkString($inString);
+            $this->inString = str_split($inString);
+            $this->checkKey($subset);
+            $this->subset = $subset;
             $this->outString = '';
             $this->count = 0;
             $this->Permutation();
             $this->customPermutation();
-        }catch(Exception $e){
-            echo 'Исключение: ', $e->getMessage(), '</br>';
-        }
-        
-    }
-
-    public function checkString($a)
-    {
-        if(!is_string($a)){
-            throw new Exception('Начальное значение должно быть строкой: '.$a .' - ' . gettype($a));
+        } catch (Exception $e) {
+            echo 'Исключение: '. $e->getMessage(). '</br>';
         }
     }
 
-    public function checkKey($k)
+    public function checkString($inString)
     {
-        if($k==0 || $k>count($this->a)){
-            throw new Exception('Значение k должно быть в диапазоне от 1 до : '.count($this->a));
+        if (!is_string($inString)) {
+            throw new Exception('Начальное значение должно быть строкой: '.$inString .' - ' . gettype($inString));
         }
     }
 
-    public function Permutation()   
+    public function checkKey($subset)
     {
-        echo "Количество возможных комбинаций по формуле комбинаторики: " .($this->getFactorial(count($this->a))/$this->getFactorial(count($this->a)-$this->k)).'</br>';
-    }
-
-    public function getFactorial($z)
-    {
-        if ($z <= 1) return 1; 
-        return $z * $this->getFactorial($z - 1);
-    }
-
-    public function getAmount()
-    {
-        $amount = 1;
-        for($i=count($this->n); $i>(count($this->n)-$this->m);$i--){
-            $amount*=$i;
+        if ($subset == 0 || $subset > count($this->inString)) {
+            throw new Exception('Значение k должно быть в диапазоне от 1 до : '.count($this->inString));
         }
-        return $amount;
+    }
+
+    public function Permutation()
+    {
+        echo "Количество возможных комбинаций по формуле комбинаторики: " .($this->getFactorial(count($this->inString))/$this->getFactorial(count($this->inString)-$this->subset)).'</br>';
+    }
+
+    public function getFactorial($number)
+    {
+        if ($number <= 1) {
+            return 1;
+        }
+        return $number * $this->getFactorial($number - 1);
     }
 
     public function customPermutation()
     {
-        for($i=0;$i<=count($this->a)-1;$i++){
+        for ($i = 0; $i <= count($this->inString)- 1; $i++) {
             $outStringPrototype = $this->outString;
-            $arrayPrototype = $this->a;
+            $arrayPrototype = $this->inString;
             $outStringPrototype .= $arrayPrototype[$i];
-            if($this->k==1){
-               echo $this->a[$i].'</br>';
-               $this->count+=1;
-            }else{
-                array_splice($arrayPrototype,$i,1);
-                $this->getCustomPermutation($arrayPrototype, $this->k-1, $outStringPrototype);
+            if ($this->subset == 1) {
+                echo $this->inString[$i].'</br>';
+                $this->count += 1;
+            } else {
+                array_splice($arrayPrototype, $i, 1);
+                $this->getCustomPermutation($arrayPrototype, $this->subset - 1, $outStringPrototype);
             }
         }
         echo 'Количество получившихся комбинаций: '.$this->count;
-        
     }
 
-    public function getCustomPermutation(array $arr, $k, $outString)
+    public function getCustomPermutation(array $arrayPrototype, $subset, $outString)
     {
-        if($k==1){
-            for($i=0;$i<=count($arr)-1;$i++){
+        if ($subset == 1) {
+            for ($i = 0; $i <= count($arrayPrototype) - 1; $i++) {
                 $outStringPrototype=$outString;
-                echo $outStringPrototype.=$arr[$i].'</br>';
+                echo $outStringPrototype.=$arrayPrototype[$i].'</br>';
                 $this->count+=1;
             }
-        }else{
-            for($j=0;$j<=count($arr)-1;$j++){
+        } else {
+            for ($j = 0; $j <= count($arrayPrototype) - 1; $j++) {
                 $outStringPrototype=$outString;
-                $arrPrototype=$arr;
-                $outStringPrototype.=$arr[$j];
-                array_splice($arrPrototype,$j,1);
-                $this->getCustomPermutation($arrPrototype,$k-1,$outStringPrototype);
+                $arrPrototype=$arrayPrototype;
+                $outStringPrototype.=$arrayPrototype[$j];
+                array_splice($arrPrototype, $j, 1);
+                $this->getCustomPermutation($arrPrototype, $subset - 1, $outStringPrototype);
             }
         }
     }
